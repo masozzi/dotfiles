1c789122498cfb24ee27e30436337165c499b9e2
Matteo Sozzi <sozziteo@googlemail.com>
2020-09-29 00:48:29 +0200

    qutebrowser: changed tabs position

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -36,7 +36,7 @@ c.colors.webpage.preferred_color_scheme = 'dark'
 # Tabs
 #
 c.tabs.favicons.scale = 1.6
-c.tabs.position = 'bottom'
+c.tabs.position = 'right'
 c.tabs.width = 46
 c.tabs.max_width = 46
 c.tabs.indicator.width = 0
