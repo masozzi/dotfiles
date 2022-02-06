6256a5b673c31b568bb3f873684ca21d7a4f6e99
Matteo Sozzi <sozziteo@googlemail.com>
2022-02-06 15:57:55 +0100

    neovim: changed theme

--- .config/nvim/lua/custom.lua
+++ .config/nvim/lua/custom.lua
@@ -6,34 +6,55 @@ local utils = require("utils")
 local Color, colors, Group, groups, style = require("colorbuddy").setup()
 
 -- Set colors
+-- Color.new("black_dark", utils.colors.black_dark)
+-- Color.new("black", utils.colors.black)
+-- Color.new("black_light", utils.colors.black_light)
+-- Color.new("black_lighter", utils.colors.black_lighter)
+-- 
+-- Color.new("red_dark", utils.colors.red_dark)
+-- Color.new("red", utils.colors.red)
+-- Color.new("red_light", utils.colors.red_light)
+-- 
+-- Color.new("green_dark", utils.colors.green_dark)
+-- Color.new("green", utils.colors.green)
+-- Color.new("green_light", utils.colors.green_light)
+-- 
+-- Color.new("yellow_dark", utils.colors.yellow_dark)
+-- Color.new("yellow", utils.colors.yellow)
+-- Color.new("yellow_light", utils.colors.yellow_light)
+-- 
+-- Color.new("blue_dark", utils.colors.blue_dark)
+-- Color.new("blue", utils.colors.blue)
+-- Color.new("blue_light", utils.colors.blue_light)
+-- 
+-- Color.new("magenta_dark", utils.colors.magenta_dark)
+-- Color.new("magenta", utils.colors.magenta)
+-- Color.new("magenta_light", utils.colors.magenta_light)
+-- 
+-- Color.new("cyan_dark", utils.colors.cyan_dark)
+-- Color.new("cyan", utils.colors.cyan)
+-- Color.new("cyan_light", utils.colors.cyan_light)
+-- 
+-- Color.new("white_dark", utils.colors.white_dark)
+-- Color.new("white", utils.colors.white)
+-- Color.new("white_light", utils.colors.white_light)
+
+Color.new("black_darker", utils.colors.black_darker)
 Color.new("black_dark", utils.colors.black_dark)
 Color.new("black", utils.colors.black)
 Color.new("black_light", utils.colors.black_light)
 Color.new("black_lighter", utils.colors.black_lighter)
 
-Color.new("red_dark", utils.colors.red_dark)
 Color.new("red", utils.colors.red)
-Color.new("red_light", utils.colors.red_light)
-
-Color.new("green_dark", utils.colors.green_dark)
-Color.new("green", utils.colors.green)
-Color.new("green_light", utils.colors.green_light)
-
-Color.new("yellow_dark", utils.colors.yellow_dark)
+Color.new("orange", utils.colors.orange)
 Color.new("yellow", utils.colors.yellow)
-Color.new("yellow_light", utils.colors.yellow_light)
+Color.new("green", utils.colors.green)
+Color.new("magenta", utils.colors.magenta)
 
 Color.new("blue_dark", utils.colors.blue_dark)
 Color.new("blue", utils.colors.blue)
 Color.new("blue_light", utils.colors.blue_light)
-
-Color.new("magenta_dark", utils.colors.magenta_dark)
-Color.new("magenta", utils.colors.magenta)
-Color.new("magenta_light", utils.colors.magenta_light)
-
-Color.new("cyan_dark", utils.colors.cyan_dark)
 Color.new("cyan", utils.colors.cyan)
-Color.new("cyan_light", utils.colors.cyan_light)
 
 Color.new("white_dark", utils.colors.white_dark)
 Color.new("white", utils.colors.white)
@@ -41,9 +62,9 @@ Color.new("white_light", utils.colors.white_light)
 
 -- Set groups
 -- function(name, fg, bg, style, guisp, blend)
-Group.new("ColorColumn", nil, colors.black_lighter, style.NONE, nil, nil)
-Group.new("Normal", colors.white, colors.black_light, style.NONE, nil, nil)
-Group.new("NonText", colors.black_lighter, nil, style.NONE, nil, nil)
+Group.new("ColorColumn", nil, colors.black, style.NONE, nil, nil)
+Group.new("Normal", colors.white, colors.black_darker, style.NONE, nil, nil)
+Group.new("NonText", colors.black, nil, style.NONE, nil, nil)
 -- Statusline colors
 Group.new("StatusLinePrimary", colors.white, colors.black, style.NONE, nil, nil)
 Group.new("StatusLineSecondary", colors.white, colors.blue, style.NONE, nil, nil)
@@ -56,9 +77,9 @@ Group.new("TabLineFill", colors.cyan, nil, style.NONE, nil, nil)
 Group.new("TabLineSel", colors.cyan, colors.blue, style.NONE, nil, nil)
 -- Vertical split color
 Group.new("VertSplit", colors.black, colors.black, style.NONE, nil, nil)
-Group.new("Whitespace", colors.black_lighter, nil, style.NONE, nil, nil)
-Group.new("LineNr", colors.black_lighter, nil, style.NONE, nil, nil)
-Group.new("CursorLine", nil, colors.black, style.NONE, nil, nil)
+Group.new("Whitespace", colors.black_light, nil, style.NONE, nil, nil)
+Group.new("LineNr", colors.black_light, nil, style.NONE, nil, nil)
+Group.new("CursorLine", nil, colors.black_dark, style.NONE, nil, nil)
 Group.new("CursorLineNr", colors.white_dark, nil, style.bold, nil, nil)
 
 --
@@ -68,29 +89,29 @@ Group.new("CursorLineNr", colors.white_dark, nil, style.bold, nil, nil)
 -- Group.new("TSAttribute")
 Group.new("TSBoolean", colors.yellow, nil, style.NONE, nil, nil)
 -- Group.new("TSCharacter")
-Group.new("TSComment", colors.white_dark, nil, style.NONE, nil, nil)
-Group.new("TSConditional", colors.yellow, nil, style.NONE, nil, nil)
-Group.new("TSConstBuiltin", colors.yellow, nil, style.NONE, nil, nil)
-Group.new("TSConstMacro", colors.red_light, nil, style.NONE, nil, nil)
-Group.new("TSConstant", colors.red_light, nil, style.NONE, nil, nil)
+Group.new("TSComment", colors.black_lighter, nil, style.NONE, nil, nil)
+Group.new("TSConditional", colors.blue, nil, style.NONE, nil, nil)
+Group.new("TSConstBuiltin", colors.blue, nil, style.NONE, nil, nil)
+Group.new("TSConstMacro", colors.red, nil, style.NONE, nil, nil)
+Group.new("TSConstant", colors.red, nil, style.NONE, nil, nil)
 -- Group.new("TSConstructor")
 Group.new("TSError", colors.white, nil, style.underline, nil, nil)
--- Group.new("TSException")
+Group.new("TSException", colors.yellow, nil, style.NONE, nil, nil)
 Group.new("TSField", colors.white, nil, style.NONE, nil, nil)
-Group.new("TSFloat", colors.red_light, nil, style.NONE, nil, nil)
-Group.new("TSFuncBuiltin", colors.red_light, nil, style.nocombine, nil, nil)
+Group.new("TSFloat", colors.red, nil, style.NONE, nil, nil)
+Group.new("TSFuncBuiltin", colors.red, nil, style.nocombine, nil, nil)
 -- Group.new("TSFuncMacro")
-Group.new("TSFunction", colors.white, nil, style.italic, nil, nil)
-Group.new("TSInclude", colors.cyan, nil, style.NONE, nil, nil)
-Group.new("TSKeyword", colors.blue_dark, nil, style.NONE, nil, nil)
-Group.new("TSKeywordFunction", colors.blue_dark, nil, style.NONE, nil, nil)
+Group.new("TSFunction", colors.blue_light, nil, style.italic, nil, nil)
+Group.new("TSInclude", colors.blue, nil, style.NONE, nil, nil)
+Group.new("TSKeyword", colors.blue, nil, style.NONE, nil, nil)
+Group.new("TSKeywordFunction", colors.blue, nil, style.NONE, nil, nil)
 Group.new("TSKeywordOperator", colors.yellow, nil, style.NONE, nil, nil)
 Group.new("TSKeywordReturn", colors.yellow, nil, style.NONE, nil, nil)
 Group.new("TSLabel", colors.cyan, nil, style.NONE, nil, nil)
 Group.new("TSMethod", colors.white, nil, style.NONE, nil, nil)
--- Group.new("TSNamespace")
+Group.new("TSNamespace", colors.white, nil, style.NONE, nil, nil)
 -- Group.new("TSNone")
-Group.new("TSNumber", colors.red_light, nil, style.NONE, nil, nil)
+Group.new("TSNumber", colors.magenta, nil, style.NONE, nil, nil)
 Group.new("TSOperator", colors.white, nil, style.NONE, nil, nil)
 Group.new("TSParameter", colors.white, nil, style.NONE, nil, nil)
 Group.new("TSParameterReference", colors.white, nil, style.NONE, nil, nil)
@@ -99,17 +120,19 @@ Group.new("TSPunctBracket", colors.white, nil, style.NONE, nil, nil)
 Group.new("TSPunctDelimiter", colors.white, nil, style.NONE, nil, nil)
 -- Group.new("TSPunctSpecial")
 Group.new("TSRepeat", colors.yellow, nil, style.NONE, nil, nil)
-Group.new("TSString", colors.magenta, nil, style.NONE, nil, nil)
-Group.new("TSStringEscape", colors.magenta_light, nil, style.NONE, nil, nil)
+Group.new("TSString",colors.green, nil, style.NONE, nil, nil)
+Group.new("TSStringEscape", colors.magenta, nil, style.NONE, nil, nil)
 Group.new("TSStringRegex", colors.blue, nil, style.NONE, nil, nil)
 -- Group.new("TSStructure")
 -- Group.new("TSSymbol")
--- Group.new("TSTag")
--- Group.new("TSTagDelimiter")
--- Group.new("TSText")
+Group.new("TSTag", colors.blue, nil, style.NONE, nil, nil)
+Group.new("TSTagAttribute", colors.cyan, nil, style.NONE, nil, nil)
+Group.new("TSTagDelimiter", colors.blue, nil, style.NONE, nil, nil)
+Group.new("TSText", colors.white, nil, style.NONE, nil, nil)
+Group.new("TSTitle", colors.magenta, nil, style.NONE, nil, nil)
 -- Group.new("TSStrike")
 -- Group.new("TSMath")
-Group.new("TSType", colors.blue, nil, style.NONE, nil, nil)
+Group.new("TSType", colors.cyan, nil, style.NONE, nil, nil)
 Group.new("TSTypeBuiltin", colors.blue, nil, style.NONE, nil, nil)
 -- Group.new("TSURI")
 Group.new("TSVariable", colors.white, nil, style.NONE, nil, nil)
@@ -119,7 +142,23 @@ Group.new("TSWarning", colors.yellow, nil, style.underline, colors.yellow, nil)
 --
 -- C
 --
-Group.new("cTSInclude", colors.red_light, nil, style.NONE, nil, nil)
+Group.new("cTSInclude", colors.red, nil, style.NONE, nil, nil)
+
+--
+-- Cpp
+--
+Group.new("cppTSInclude", colors.red, nil, style.NONE, nil, nil)
+-- Group.new("cppTSKeyword", colors.blue_dark, nil, style.NONE, nil, nil)
+
+--
+-- Go
+--
+Group.new("goTSMethod", colors.blue_light, nil, style.NONE, nil, nil)
+
+--
+-- Html
+--
+Group.new("htmlTSTitle", colors.white, nil, style.NONE, nil, nil)
 
 --
 -- Yaml
@@ -129,8 +168,8 @@ Group.new("yamlTSField", colors.blue, nil, style.NONE, nil, nil)
 --
 -- Nvimtree
 --
-Group.new("NvimTreeEndOfBuffer", colors.black, colors.black, style.NONE, nil, nil)
-Group.new("NvimTreeNormal", colors.fg, colors.black, style.NONE, nil, nil)
-Group.new("NvimTreeVertSplit", colors.black, colors.black, style.NONE, nil, nil)
-Group.new("NvimTreeStatusLine", colors.black, colors.black, style.NONE, nil, nil)
-Group.new("NvimTreeStatusLineNc", colors.black, colors.black, style.NONE, nil, nil)
+Group.new("NvimTreeEndOfBuffer", colors.black_dark, colors.black_dark, style.NONE, nil, nil)
+Group.new("NvimTreeNormal", colors.fg, colors.black_dark, style.NONE, nil, nil)
+Group.new("NvimTreeVertSplit", colors.black_dark, colors.black_dark, style.NONE, nil, nil)
+Group.new("NvimTreeStatusLine", colors.black_dark, colors.black_dark, style.NONE, nil, nil)
+Group.new("NvimTreeStatusLineNc", colors.black_dark, colors.black_dark, style.NONE, nil, nil)
--- .config/nvim/lua/utils.lua
+++ .config/nvim/lua/utils.lua
@@ -1,38 +1,61 @@
 local M = {}
 
+-- M.colors = {
+-- 	black_dark = "#111015",
+-- 	black = "#16161F",
+-- 	black_light = "#1A1B27",
+-- 	black_lighter = "#363B54",
+-- 
+-- 	red_dark = "#DB4B4B",
+-- 	red = "#FF5370",
+-- 	red_light = "#F7768E",
+-- 
+-- 	green_dark = "#007A75",
+-- 	green = "#1ABC9C",
+-- 	green_light = "#73DACA",
+-- 
+-- 	yellow_dark = "#FF9E64",
+-- 	yellow = "#FFDB69",
+-- 	yellow_light = "#FFEB40",
+-- 
+-- 	blue_dark = "#7AA2F7",
+-- 	blue = "#9ABDF5",
+-- 	blue_light = "#7DCFFF",
+-- 
+-- 	magenta_dark = "#B267E6",
+-- 	magenta = "#9D7CD8",
+-- 	magenta_light = "#BB9AF7",
+-- 
+-- 	cyan_dark = "#0DA0BA",
+-- 	cyan = "#0DB9D7",
+-- 	cyan_light = "#3DD8F3",
+-- 
+-- 	white_dark = "#787C99",
+-- 	white = "#C0CAF5",
+-- 	white_light = "#A9B1D6"
+-- }
+
 M.colors = {
-	black_dark = "#111015",
-	black = "#16161F",
-	black_light = "#1A1B27",
-	black_lighter = "#363B54",
+	black_darker = "#2E3440",
+	black_dark = "#323846",
+	black = "#3B4252",
+	black_light = "#434C5E",
+	black_lighter = "#616E88",
 
-	red_dark = "#DB4B4B",
-	red = "#FF5370",
-	red_light = "#F7768E",
+	red = "#BF616A",
+	orange = "#D08770",
+	yellow = "#EBCB8B",
+	green = "#A3BE8C",
+	magenta = "#B48EAD",
 
-	green_dark = "#007A75",
-	green = "#1ABC9C",
-	green_light = "#73DACA",
+	blue_dark = "#5E91AC",
+	blue = "#81A1C1",
+	blue_light = "#88C0D0",
+	cyan = "#8FBCBB",
 
-	yellow_dark = "#FF9E64",
-	yellow = "#FFDB69",
-	yellow_light = "#FFEB40",
-
-	blue_dark = "#7AA2F7",
-	blue = "#9ABDF5",
-	blue_light = "#7DCFFF",
-
-	magenta_dark = "#B267E6",
-	magenta = "#9D7CD8",
-	magenta_light = "#BB9AF7",
-
-	cyan_dark = "#0DA0BA",
-	cyan = "#0DB9D7",
-	cyan_light = "#3DD8F3",
-
-	white_dark = "#787C99",
-	white = "#C0CAF5",
-	white_light = "#A9B1D6"
+	white_dark = "#D9DEE9",
+	white = "#E5E9F0",
+	white_light = "#ECEFF4"
 }
 
 M.hide_statusline = function()
